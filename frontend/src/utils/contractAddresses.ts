b7c5deee74f6971698783747f9c7057574b6349c
Saarule <saaryazdi@gmail.com>
2024-08-06 16:11:39 +0300

    Update contractAddresses.ts

--- frontend/src/utils/contractAddresses.ts
+++ frontend/src/utils/contractAddresses.ts
@@ -1,5 +1,5 @@
 export const CONTRACT_ADDRESSES: { [chainId: number]: string } = {
   1: '0x...',  // Ethereum Mainnet
   11155420: '0xbE78bFcF8532315550DAbb7E80dc705B0F6F9842',  // Optimism Sepolia
-  // Add more addresses for different chains
+  36630: '0xEe3e274AF008E708DB9e85554aBFeC7B259E0AAF',  // LOLZ Testnet
 };
